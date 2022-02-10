7a306653180eab7b4984c834b769f2012e1c9da3
RaouiRahmeni <raouirahmeni@gmail.com>
2022-02-10 17:34:42 +0100

    i updated the resume2

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Raoui_Rahmeni_Resume_Ang.pdf";
+import pdf from "../../Assets/Raoui_Rahmeni's_Resume_Ang.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
